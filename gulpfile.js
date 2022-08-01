9cfb4d8d89e268fb97bd9ed03e7b126eab45ef3e
Oleksandr Spievakov <alexandr.pobeda99@gmail.com>
2022-08-01 11:13:12 -0400

    added all fileInclude functionality

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,10 @@
 const {src, dest} =require('gulp');
 
+const fileInclude = require('gulp-file-include');
+
 const html = () => {
    return src('./src/html/*.html')
+   .pipe(fileInclude())
    .pipe(dest('./public'));
 };
 
