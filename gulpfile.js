7786dc3ce553df29828ceda20f220e8d4094f696
Oleksandr Spievakov <alexandr.pobeda99@gmail.com>
2022-08-01 11:17:40 -0400

    added htmlmin to the project

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,14 @@
 const {src, dest} =require('gulp');
 
 const fileInclude = require('gulp-file-include');
+const htmlmin = require('gulp-htmlmin');
 
 const html = () => {
    return src('./src/html/*.html')
    .pipe(fileInclude())
+   .pipe(htmlmin({
+      collapseWhitespace: true
+   }))
    .pipe(dest('./public'));
 };
 
