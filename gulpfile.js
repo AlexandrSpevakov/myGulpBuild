4ab06a021927fb52607df1309f7887915926ccdb
Oleksandr Spievakov <alexandr.pobeda99@gmail.com>
2022-08-01 11:20:51 -0400

    added gulp-size

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,16 @@ const {src, dest} =require('gulp');
 
 const fileInclude = require('gulp-file-include');
 const htmlmin = require('gulp-htmlmin');
+const size = require('gulp-size');
 
 const html = () => {
    return src('./src/html/*.html')
    .pipe(fileInclude())
+   .pipe(size({title: 'before'}))
    .pipe(htmlmin({
       collapseWhitespace: true
    }))
+   .pipe(size({title: 'after'}))
    .pipe(dest('./public'));
 };
 
