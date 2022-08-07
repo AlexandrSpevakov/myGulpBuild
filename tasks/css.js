8fd6706627f0924654987d02f733b629a4204678
Oleksandr Spievakov <alexandr.pobeda99@gmail.com>
2022-08-07 14:31:50 -0400

    added if to css.js

--- tasks/css.js
+++ tasks/css.js
@@ -11,12 +11,13 @@ import autoPrefixer from 'gulp-autoprefixer';
 import csso from 'gulp-csso';
 import rename from 'gulp-rename';
 import sourcemaps from 'gulp-sourcemaps';
+import gulpIf from 'gulp-if';
 // import size from 'gulp-size';
 
 // Task
 export default () => {
    return gulp.src(path.css.src)
-   .pipe(sourcemaps.init())
+   .pipe(gulpIf(app.isDev, sourcemaps.init()))
    .pipe(concat('main.css'))
    .pipe(cssImport())
    .pipe(autoPrefixer())
@@ -25,6 +26,6 @@ export default () => {
    .pipe(rename(app.rename))
    .pipe(csso())
    // .pipe(size({title: 'main.min.css'}))
-   .pipe(sourcemaps.write())
+   .pipe(gulpIf(app.isDev, sourcemaps.write()))
    .pipe(gulp.dest(path.css.dest))
 }
