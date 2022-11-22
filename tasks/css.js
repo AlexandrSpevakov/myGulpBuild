fa5fe8e223e3702f778b3dd0d0b4e7d343387b04
Oleksandr Spievakov <alexandr.pobeda99@gmail.com>
2022-11-22 07:47:17 -0500

    refactor: change oreder of imports in css task

--- tasks/css.js
+++ tasks/css.js
@@ -1,31 +1,34 @@
-import gulp from 'gulp';
+import gulp from "gulp";
 
 // Config
-import path from '../config/path.js';
-import app from '../config/app.js';
+import path from "../config/path.js";
+import app from "../config/app.js";
 
 // Plugins
-import concat from 'gulp-concat';
-import cssImport from 'gulp-cssimport';
-import autoPrefixer from 'gulp-autoprefixer';
-import csso from 'gulp-csso';
-import rename from 'gulp-rename';
-import sourcemaps from 'gulp-sourcemaps';
-import gulpIf from 'gulp-if';
+import autoPrefixer from "gulp-autoprefixer";
+import csso from "gulp-csso";
+import rename from "gulp-rename";
+import sourcemaps from "gulp-sourcemaps";
+import gulpIf from "gulp-if";
+import concat from "gulp-concat";
+import cssImport from "gulp-cssimport";
 // import size from 'gulp-size';
 
 // Task
 export default () => {
-   return gulp.src(path.css.src)
-   .pipe(gulpIf(app.isDev, sourcemaps.init()))
-   .pipe(concat('main.css'))
-   .pipe(cssImport())
-   .pipe(autoPrefixer())
-   .pipe(gulp.dest(path.css.dest))
-   // .pipe(size({title: 'main.css'}))
-   .pipe(rename(app.rename))
-   .pipe(csso())
-   // .pipe(size({title: 'main.min.css'}))
-   .pipe(gulpIf(app.isDev, sourcemaps.write('.')))
-   .pipe(gulp.dest(path.css.dest))
-}
+  return (
+    gulp
+      .src(path.css.src)
+      .pipe(gulpIf(app.isDev, sourcemaps.init()))
+      .pipe(concat("main.css"))
+      .pipe(cssImport())
+      .pipe(autoPrefixer())
+      .pipe(gulp.dest(path.css.dest))
+      // .pipe(size({title: 'main.css'}))
+      .pipe(rename(app.rename))
+      .pipe(csso())
+      // .pipe(size({title: 'main.min.css'}))
+      .pipe(gulpIf(app.isDev, sourcemaps.write(".")))
+      .pipe(gulp.dest(path.css.dest))
+  );
+};
